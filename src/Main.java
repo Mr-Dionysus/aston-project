3fd0633d4342dfd5cf2343851e37e0f0b7d00ef2
Maksim Ushakov <chaosfirebird@yandex.ru>
2024-08-09 16:49:54 +0300

    Initial Working Release

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("What class's objects do you want to use? Write 'stop' to stop");
-            String classObjects = scanner.nextLine();
-            if (classObjects.equals("stop")) {
-                break;
-            }
+//        while (true) {
+//            System.out.println("What class's objects do you want to use? Write 'stop' to stop");
+//            String classObjects = scanner.nextLine();
+//            if (classObjects.equals("stop")) {
+//                break;
+//            }
+//
+//            System.out.println("Write a length of array. Write '0' to stop");
+//            int arrLength = Integer.parseInt(scanner.nextLine());
+//            if (arrLength == 0) {
+//                break;
+//            }
+//
+//            System.out.println("Do you want to add new data in array via file, by random, or via console? Write 'stop' to stop");
+//            String addDataOptions = scanner.nextLine();
+//            if (addDataOptions.equals("stop")) {
+//                break;
+//            }
+//        }
+        int[] mergeSortExample = {1,2,9,8,4,5};
 
-            System.out.println("Write a length of array. Write '0' to stop");
-            int arrLength = Integer.parseInt(scanner.nextLine());
-            if (arrLength == 0) {
-                break;
-            }
 
-            System.out.println("Do you want to add new data in array via file, by random, or via console? Write 'stop' to stop");
-            String addDataOptions = scanner.nextLine();
-            if (addDataOptions.equals("stop")) {
-                break;
-            }
-        }
     }
 }
